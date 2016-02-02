426684d2b939d3655ef34225a61f987bcf98e4eb
kolektiv <andrew@xyncro.com>
2016-02-02 00:05:16 +0000

    using a common configuration builder as with router, from core

---
+++ src/Freya.Machines.Http/Machines.Http.fs
@@ -0,0 +1,93 @@
+﻿module Freya.Machines.Http
+
+open Freya.Core
+
+(* Configuration
+
+   Types and functions for user configuration of an HTTP Machine, based
+   on configuration through a computation expression with various custom
+   operations, and taking advantage of static type inference to allow a concise
+   and flexible API (for example, accepting a Method or list of Methods
+   transparently). *)
+
+(* Types
+
+   The basic types of configuration, the HttpMachine (configuration) function,
+   and the actual configuration data threaded through configuration functions.
+
+   The function itself is defined as a single case discriminated union so that
+   it can have static members, allowing it to take part in the static inference
+   approaches of the basic Freya function, and Pipelines. *)
+
+type HttpMachine =
+    | HttpMachine of (HttpMachineSettings -> unit * HttpMachineSettings)
+
+ and HttpMachineSettings =
+    | HttpMachineSettings of string list
+
+    static member settings_ =
+        (fun (HttpMachineSettings x) -> x), (HttpMachineSettings)
+
+(* Functions
+
+   Functions implementing common monadic operations on the Router type,
+   which will be used to implement the computation expression builder.
+
+   Additionally some extensions to the basic requirements for a computation
+   expression are included, including mapping over the inner state. *)
+
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module HttpMachine =
+
+    (* Common *)
+
+    let init _ : HttpMachine =
+        HttpMachine (fun c ->
+            (), c)
+
+    let bind (m: HttpMachine, f: unit -> HttpMachine) : HttpMachine =
+        HttpMachine (fun c ->
+            let (HttpMachine m) = m
+            let (HttpMachine f) = f ()
+
+            (), snd (f (snd (m c))))
+
+    (* Custom *)
+
+    let inline map (m: HttpMachine, f: HttpMachineSettings -> HttpMachineSettings) : HttpMachine =
+        HttpMachine (fun c ->
+            let (HttpMachine m) = m
+
+            (), f (snd (m c)))
+
+    (* Operations *)
+
+    let operations =
+        { Configuration.Operations.Init = init
+          Configuration.Operations.Bind = bind }
+
+(* Builder
+
+   Computation expression builder for configuring the HTTP Machine, providing a
+   simple type-safe syntax and static inference based overloads of single
+   functions. *)
+
+type HttpMachineBuilder () =
+    inherit Configuration.Builder<HttpMachine> (HttpMachine.operations)
+
+(* Expressions
+
+   Computation expressions, instances of the HTTP Machine builder. The fully
+   named instance, freyaHttpMachine is aliased to freyaMachine to provide the
+   possibility of more concise code when only one kind of machine is in scope.
+
+   This naming also matches the original single form approach to machines, and
+   provides backwards compatibility. *)
+
+let freyaHttpMachine =
+    HttpMachineBuilder ()
+
+let freyaMachine =
+    freyaHttpMachine
+
--- src/Freya.Machines/Machines.fs
+++ src/Freya.Machines/Machines.fs
@@ -4,26 +4,16 @@ open Freya.Core
 open Freya.Core.Operators
 open Hephaestus
 
-(* Types
-
-   Common types for functions, etc. as part of HTTP machines. The
-   Decision<'a> type is analogous to the basic Hephaestus decision type, and
-   is used by Freya users, but mapped to the Hephaestus decision type before
-   the specifications for the components and models are used. *)
+(* Types *)
 
 type Decision<'a> =
     | Function of Freya<'a>
     | Literal of 'a
 
-(* Decisions
-
-   Type conversions between Hephaestus specific types (the convention of
-   Right = true and Left = false is followed here, Hephaestus has a value
-   neutral decision scheme) and Freya types (particularly the mapping of the
-   two Decision<'a> implementations). *)
+(* Decisions *)
 
 [<RequireQualifiedAccess>]
-module internal Decision =
+module Decision =
 
     type private Decision =
         Hephaestus.Decision<State>
