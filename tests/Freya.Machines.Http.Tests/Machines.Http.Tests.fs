f41bf637dc28235aa17e2223c09ea2b9eb01bd81
kolektiv <andrew@xyncro.com>
2016-03-15 22:52:51 +0000

    test operations element

--- tests/Freya.Machines.Http.Tests/Machines.Http.Tests.fs
+++ tests/Freya.Machines.Http.Tests/Machines.Http.Tests.fs
@@ -788,3 +788,84 @@ module Conflict =
         verify conflictSetup dynamicMachine [
             Response.statusCode_ => Some 409
             Response.reasonPhrase_ => Some "Conflict" ]
+
+(* Operation
+
+   Verification that the Operation element behaves as expected given suitable
+   input, including verification that the operation to be run is executed and
+   that the result of the operation influences the response path. *)
+
+module Operation =
+
+    (* Operation *)
+
+    [<Fact>]
+    let ``machine processes operation correctly`` () =
+
+        let test_ =
+            State.value_<string> "test"
+
+        let setup =
+            Request.method_ .= POST
+
+        (* Success *)
+
+        let success =
+            freya {
+                do! Freya.Optic.set test_ (Some "test") }
+
+        let successMachine =
+            freyaMachine {
+                doPost success
+                methods POST }
+
+        verify setup successMachine [
+            Response.statusCode_ => Some 200
+            Response.reasonPhrase_ => Some "OK"
+            test_ => Some "test" ]
+
+        (* Failure *)
+
+        let failure =
+            freya {
+                do! Freya.Optic.set test_ (Some "test")
+                return false }
+
+        let failureMachine =
+            freyaMachine {
+                doPost failure
+                methods POST }
+
+        verify setup failureMachine [
+            Response.statusCode_ => Some 500
+            Response.reasonPhrase_ => Some "Internal Server Error"
+            test_ => Some "test" ]
+
+    (* Completed *)
+
+    [<Fact>]
+    let ``machine handles completed correctly`` () =
+
+        let setup =
+            Request.method_ .= POST
+
+        (* Completed *)
+
+        let completedMachine =
+            freyaMachine {
+                methods POST }
+
+        verify setup completedMachine [
+            Response.statusCode_ => Some 200
+            Response.reasonPhrase_ => Some "OK" ]
+
+        (* Uncompleted *)
+
+        let uncompletedMachine =
+            freyaMachine {
+                completed false
+                methods POST }
+
+        verify setup uncompletedMachine [
+            Response.statusCode_ => Some 202
+            Response.reasonPhrase_ => Some "Accepted" ]
