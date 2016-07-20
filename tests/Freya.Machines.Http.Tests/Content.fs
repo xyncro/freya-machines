e9d927aaa7df40872f803cd103ec9b9e013a4a30
kolektiv <andrew@xyncro.com>
2016-07-20 09:30:32 +0100

    further tests for patch and content

---
+++ tests/Freya.Machines.Http.Tests/Content.fs
@@ -0,0 +1,53 @@
+﻿module Freya.Machines.Http.Tests.Content
+
+open Freya.Core.Operators
+open Freya.Machines.Http
+open Freya.Optics.Http
+open Freya.Testing
+open Freya.Testing.Operators
+open Freya.Types.Http
+open Xunit
+
+(* Content
+
+   Verification that the Content element behaves as expected given suitable
+   input. *)
+
+(* Content-Length *)
+
+[<Fact>]
+let ``machine handles absent content length correctly`` () =
+
+    let setup =
+            (Request.method_ .= POST)
+
+    (* Content-Length *)
+
+    let machine =
+        freyaHttpMachine {
+            methods POST }
+
+    verify setup machine [
+        Response.statusCode_ => Some 411
+        Response.reasonPhrase_ => Some "Length Required" ]
+
+(* Content-Type *)
+
+[<Fact>]
+let ``machine handles unsupported content type correctly`` () =
+
+    let setup =
+            (Request.method_ .= POST)
+         *> (Request.Headers.contentLength_ .= Some (ContentLength 0))
+         *> (Request.Headers.contentType_ .= Some (ContentType (MediaType.Text)))
+
+    (* Content-Length *)
+
+    let machine =
+        freyaHttpMachine {
+            acceptableMediaTypes MediaType.Json
+            methods POST }
+
+    verify setup machine [
+        Response.statusCode_ => Some 415
+        Response.reasonPhrase_ => Some "Unsupported Media Type" ]
