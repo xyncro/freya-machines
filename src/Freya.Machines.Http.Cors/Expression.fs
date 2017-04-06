569db88079e777c75ccb0660832177c88c1180d9
Marcus Griep <mgriep@cimpress.com>
2017-04-06 02:26:29 +0200

    Add documentation to the HTTP machine

--- src/Freya.Machines.Http.Cors/Expression.fs
+++ src/Freya.Machines.Http.Cors/Expression.fs
@@ -23,12 +23,15 @@ module Syntax =
 
     type HttpMachineBuilder with
 
+        /// Includes the CORS components in the Freya HTTP machine.
         [<CustomOperation ("cors", MaintainsVariableSpaceUsingBind = true)>]
         member inline x.Cors (m) =
             x.Using (m, Use.cors)
 
     type HttpMachineBuilder with
 
+        /// Determines whether the CORS components are enabled for the current
+        /// request.
         [<CustomOperation ("corsEnabled", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsEnabled (m, a) =
             HttpMachine.set (m, Extension.enabled_, Decision.infer a)
@@ -37,26 +40,35 @@ module Syntax =
 
     type HttpMachineBuilder with
 
+        /// Specifies the (list of) valid CORS origins.
         [<CustomOperation ("corsOrigins", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsOrigins (m, a) =
             HttpMachine.set (m, Properties.Resource.origins_, Origins.infer a)
 
+        /// Specifies the (list of) valid CORS methods, if different from the
+        /// methods specified with `methods`.
         [<CustomOperation ("corsMethods", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsMethods (m, a) =
             HttpMachine.set (m, Properties.Resource.methods_, Methods.infer a)
 
+        /// Specifies the (list of) valid CORS headers.
         [<CustomOperation ("corsHeaders", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsHeaders (m, a) =
             HttpMachine.set (m, Properties.Resource.headers_, Headers.infer a)
 
+        /// Specifies the (list of) headers that a browser is allowed to access.
         [<CustomOperation ("corsExposedHeaders", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsExposedHeaders (m, a) =
             HttpMachine.set (m, Properties.Resource.exposedHeaders_, Headers.infer a)
 
+        /// Specifies whether the CORS request is allowed to use the user's
+        /// credentials of the server
         [<CustomOperation ("corsSupportsCredentials", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsSupportsCredentials (m, a) =
             HttpMachine.set (m, Properties.Resource.supportsCredentials_, Decision.infer a)
 
+        /// Specifies the maximum timespan in seconds that a client can cache
+        /// CORS header data before re-validating.
         [<CustomOperation ("corsMaxAge", MaintainsVariableSpaceUsingBind = true)>]
         member inline __.CorsMaxAge (m, a) =
             HttpMachine.set (m, Properties.Resource.maxAge_, TimeSpan.infer a)
